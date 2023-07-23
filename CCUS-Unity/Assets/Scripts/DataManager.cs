e04ee14e7dc97ec878cb685f104c73ce73d3f903
Doggitoz <cpwheeler4@gmail.com>
2023-07-22 19:00:54 -0700

    Fleshed out Data Manager

--- CCUS-Unity/Assets/Scripts/DataManager.cs
+++ CCUS-Unity/Assets/Scripts/DataManager.cs
@@ -17,12 +17,14 @@ public class DataManager : MonoBehaviour
     }
     #endregion
 
-    [SerializeField] float startingMoney;
-    [SerializeField] float startingCarbon;
+    [SerializeField] int startingMoney;
+    [SerializeField] int startingCarbon;
     [SerializeField] int startingYear;
-    float money;
-    float carbon;
+    int money;
+    int carbon;
     int year;
+    int storageCapacity;
+    int stored;
 
     private void Awake()
     {
@@ -31,31 +33,70 @@ public class DataManager : MonoBehaviour
     }
 
     /// <summary>
-    /// Sets the values of money and carbon to their default values.
+    /// Sets the data values to their default values.
     /// </summary>
     public void ResetData()
     {
         money = startingMoney;
         carbon = startingCarbon;
         year = startingYear;
+        storageCapacity = 0;
+        stored = 0;
     }
 
     /// <summary>
-    /// Increase or decrease the current simulation money balance. Positive value adds, negative value subtracts.
+    /// Increase or decrease the current simulation money balance. 
+    /// Positive value adds, negative value subtracts.
     /// </summary>
     /// <param name="value"></param>
-    public void AddMoney(float value)
+    public void AdjustMoney(int value)
     {
         money += value;
     }
 
     /// <summary>
-    /// Increase or decrease the current simulation carbon presence. Positive value adds, negative value subtracts.
+    /// Increase or decrease the current simulation carbon presence. 
+    /// Positive value adds, negative value subtracts.
     /// </summary>
     /// <param name="value"></param>
-    public void AddCarbon(float value)
+    public void AdjustCarbon(int value)
     {
         carbon += value;
+    }
+
+    /// <summary>
+    /// Increase or decrease the current simulation carbon storage size. If decreased, release all carbon over capacity.
+    /// Positive value adds, negative value subtracts.
+    /// </summary>
+    /// <param name="value"></param>
+    public void AdjustStorageSize(int value)
+    {
+        storageCapacity += value;
+
+        // If storage size decreased, release over capacity carbon into the atmosphere
+        if (stored > storageCapacity)
+        {
+            AdjustCarbon(stored - storageCapacity);
+            SetStored(storageCapacity);
+        }
+
+    }
+
+    /// <summary>
+    /// Increase or decrease the current amonut of carbon stored. If the amount of carbon added goes over the storage capacity, the carbon is released rather than 
+    /// stored. 
+    /// Positive value adds, negative value subtracts.
+    /// </summary>
+    /// <param name="value"></param>
+    public void AdjustStored(int value)
+    {
+        if (stored + value > storageCapacity)
+        {
+            value = storageCapacity - stored;
+            SetStored(storageCapacity);
+            AdjustCarbon(value);
+        }
+        stored += value;
     }
 
     /// <summary>
@@ -72,16 +113,21 @@ public class DataManager : MonoBehaviour
     /// Returns the current money balance of the simulation.
     /// </summary>
     /// <returns></returns>
-    public float GetMoney()
+    public int GetMoney()
     {
         return money;
+    }
+
+    public void SetMoney(int value)
+    {
+
     }
 
     /// <summary>
     /// Returns the current carbon value of the simulation.
     /// </summary>
     /// <returns></returns>
-    public float GetCarbon()
+    public int GetCarbon()
     {
         return carbon;
     }
@@ -95,5 +141,31 @@ public class DataManager : MonoBehaviour
         return year;
     }
 
+    /// <summary>
+    /// Returns the current amount of stored carbon of the simulation.
+    /// </summary>
+    /// <returns></returns>
+    public int GetStored()
+    {
+        return stored;
+    }
+
+    /// <summary>
+    /// Sets the current amount of stored carbon of the simulation.
+    /// </summary>
+    /// <returns></returns>
+    public void SetStored(int val)
+    {
+        stored = val;
+    }
+
     #endregion
+}
+
+
+// Plan is to eventually make a priority queue for different requests. I.E. Processing all money requests, then carbon storage, then carbon additions, etc
+// This will also allow us to track what type of source these requests are made from, highly benefiting the contract system
+class ModifyRequest
+{
+
 }
